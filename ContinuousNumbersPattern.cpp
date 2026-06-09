7b5c2a9032eeef344632ed7c64dd1c76c709f2b2
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-09 13:47:50 +0530

    Continuous number pattern

---
+++ ContinuousNumbersPattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row,col,count=1;
+   for(row=1;row<=5;row++)
+     {
+      
+       for(col=1;col<=5;col++)
+       {
+        cout<<count<<"  ";
+        count=count+1;
+       }
+       cout<<endl;
+     }
+ }    
