05e29dbe8b3b4e4c4db7dd30b149f64f6977238c
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-13 22:02:43 +0530

    Palindromic number pyramid

---
+++ PalindromicNumberPyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int row,col;
+for(row=1; row<=4; row++)
+{
+for(col=1; col<=4-row; col++)
+cout<<"  ";
+ 
+ for(col=1; col<=row; col++)
+ cout<<col<<"  ";
+ for(col=row-1; col>=1;col--)
+ cout<<col<<" ";
+ cout<<endl;
+ }
+ }
