6ad47dc390f3361715b974fd5e11d8d4b1e8b35d
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-06-13 22:03:45 +0530

    Butterfly print

---
+++ Butterfly Print.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, row, col;
+    cin >> n;
+
+    // Upper half
+    for(row = 1; row <= n; row++)
+    {
+        for(col = 1; col <= row; col++)
+            cout << "*";
+
+        for(col = 1; col <= 2*(n-row); col++)
+            cout << " ";
+
+        for(col = 1; col <= row; col++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    // Lower half
+    for(row = n; row >= 1; row--)
+    {
+        for(col = 1; col <= row; col++)
+            cout << "*";
+
+        for(col = 1; col <= 2*(n-row); col++)
+            cout << " ";
+
+        for(col = 1; col <= row; col++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
